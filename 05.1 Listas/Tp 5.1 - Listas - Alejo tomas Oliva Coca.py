41c9dae8689b8c2d7e52b73bff714b4668075dee
AlejoOliva <alejooliva070@gmail.com>
2025-04-23 18:18:39 -0300

    Add files via upload

---
+++ 05.1 Listas/Tp 5.1 - Listas - Alejo tomas Oliva Coca.py
@@ -0,0 +1,62 @@
+# Actividad 1
+lista_1_al_100 = list(range(1,101))
+
+# Actividad 2
+lista_5_elemntos = ["hola", "auto", "casa", "avion", "hotel"]
+
+print("Actividad 2 ===",lista_5_elemntos[3])
+
+# Actividad 3
+lista_vacia = []
+
+lista_vacia.append("vaca")
+lista_vacia.append("perro")
+lista_vacia.append("gato")
+
+print("Actividad 3 ===",lista_vacia)
+
+# Actividad 4
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[3] = "oso"
+
+print("Actividad 4 ===",animales)
+
+# Actividad 5
+# El programa remueve el maximo numero de la lista
+
+# Actividad 6
+lista_10_al_30 = list(range(10,31,5))
+
+print("Actividad 6 ===",lista_10_al_30)
+
+# Actividad 7
+autos = ["sedan","polo","suran","gol"]
+autos[1] = "palio"
+autos[2] = "vento"
+
+print("Actividad 7 ===",autos)
+
+# Actividad 8
+dobles = []
+
+dobles.append(5 * 2)
+dobles.append(10)
+dobles.append(15)
+
+print("Actividad 8 ===",dobles)
+
+# Actividad 9
+compras = [["pan", "leche"],["arroz", "fideos", "salsa"], ["agua"]]
+
+compras[2].append("jugo")
+compras[1][1] = "tallarines"
+compras[0].remove("pan")
+
+
+print("Actividad 9 ===",compras)
+
+# Actividad 10
+lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+
+print("Actividad 10 ===",lista_anidada)
